e928d1b09e97925408e0c65a8169f2c69a1715c0
jpphang <joyanna_phang1@my.vcccd.edu>
2024-06-28 00:09:32 -0700

    4-3-i

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ def main():
 
     numbers = [0] * 5
     for i in range(len(numbers)):
-        numbers[i] = int(input('Enter a value: '))
+        numbers[i] = int(input('Enter a value:'))
         
     totla = 0
     
